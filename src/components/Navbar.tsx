48488ab841a7b6cd33b275d564f702eb591b083b
Aiden Hammond <amh37629@uga.edu>
2023-04-02 14:07:19 -0400

    navbar+post style changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'
-import LG2S from '../assets/LG2S.jpeg'
+import LG2S from '../assets/LG2S.png'
 import MEMESat_1_Logo from '../assets/MEMESAT-1.png'
-import { useState } from 'react';
-import { retrieveToken } from './Security';
 import { useAuth } from '../AuthContext';
 
 const Navbar: React.FC = () => {
@@ -13,34 +11,35 @@ const Navbar: React.FC = () => {
     return (
         <nav>
             <div className={"logos"}>
-            <img className={"meme1_logo"} src={MEMESat_1_Logo} alt="MEMESat-1 logo"/>
-                <Link to="https://letsgo2space.com/"> 
-                    <img className={"lg2s_logo"} src={LG2S} alt="Lets Go To Space logo"/>
+                <img className={"meme1_logo"} src={MEMESat_1_Logo} alt="MEMESat-1 logo"/>
+                <Link to="https://letsgo2space.com/" target="_blank"> 
+                    <img className="lg2s_logo" src={LG2S} alt="Lets Go To Space logo"/>
                 </Link>
             </div>
             <div className={"redirects"}>
                 <div>
-                    <Link to="/" className={"link"}>Home</Link>
+                    <Link to="/" className="link">Home</Link>
                 </div>
                 <div>
-                    <Link to="/posts" className={"link"}>Posts</Link>
+                    <Link to="/posts" className="link">Posts</Link>
                 </div>
-                {isLoggedIn ? (
-                    <div>
-                        <Link to="/" className={"link"}>
-                            <button onClick={logout}>Logout</button>
-                        </Link>
-                    </div>
-                ) : (
-                    <div>
-                        <Link to="/login" className={"link"}>Login</Link>
-                    </div>
-                )}
                 {isLoggedIn && (
                     <div>
                         <Link to="/new-post" className={"link"}>New Post</Link>
                     </div>
                 )}
+                {isLoggedIn ? (
+                    <div>
+                        <Link to="/" className="link" onClick={logout}>
+                            Logout
+                        </Link>
+                    </div>
+                ) : (
+                    <div>
+                        <Link to="/login" className="link">Login</Link>
+                    </div>
+                )}
+                
             </div>
         </nav> 
     );
