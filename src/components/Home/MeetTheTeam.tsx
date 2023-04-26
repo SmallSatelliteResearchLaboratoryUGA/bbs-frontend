dbb6cf4314d99c393405cf6c6589a6edfe262b6b
Aiden Hammond <amh37629@uga.edu>
2023-04-26 12:11:40 -0400

    init, v1

--- src/components/Home/MeetTheTeam.tsx
+++ src/components/Home/MeetTheTeam.tsx
@@ -1,20 +1,34 @@
 import "../../styles/Home.css"
 import React from 'react';
-import { Grid } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import MemeTeam from "../../assets/meme team.jpg"
 
 const MeetTheTeam: React.FC = () => {
     // TODO
     return (
         <div className="MeetTheTeamContainer">
-            <Grid container spacing={1}>
-                <Grid item xs={12}>
-                    <h2 id="meet-the-team-title">
-                        Meet the team
-                    </h2>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Box
+                        height="100%"
+                        display="flex"
+                        justifyContent="center"
+                        flexDirection="column"
+                        >
+                        <h2 id="meet-the-team-title">
+                            Meet the team
+                        </h2>
+                    </Box>
                 </Grid>
-                <Grid item xs={12}>
-                    <img src={MemeTeam} id="memeteam"/>
+                <Grid item xs={6} justifyContent="center">
+                    <Box 
+                        height="100%"
+                        display="flex"
+                        
+                        width="fit-content"
+                    >
+                        <img src={MemeTeam} id="memeteam"/>
+                    </Box>
                 </Grid>
             </Grid>
         </div>
