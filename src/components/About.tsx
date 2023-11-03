ca119a115eabe62fa4ec7bbc096dd71cfa947f2e
QuintonHeald <qbh18213@uga.edu>
2023-11-03 18:37:36 -0400

    Added AboutBox + AboutBox CSS

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import "../styles/About.css"
+import AboutBox from './AboutBox';
 import MemeTeam from "../assets/meme team.jpg"
 
 const About: React.FC = () => {
@@ -13,33 +14,9 @@ const About: React.FC = () => {
       <h3 className="about-statement" id="mission-statement"> 
         Mission Statement
       </h3>
-        <div className="box">
-          <div className="inner">
-            <h2>About BBS</h2>
-            <p>
-                  "Link to tutorial(?)"
-            </p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="inner">
-            <h2>MEMESat-1 Mission Statement</h2>
-            <p>
-                  "Mission Statement(?)"
-            </p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="inner">
-            <h2>Meet the Team</h2>
-            <p>
-                  "Team description or link to page of individual members(?)"
-            </p>
-          </div>
-          <div>
-            <img src={MemeTeam} id="memeteam"/>
-          </div>
-        </div>
+      <AboutBox title="About BBS" description="Link to tutorial(?)"/>
+      <AboutBox title="MEMESat-1 Mission Statement" description="Mission Statement(?)"/>
+      <AboutBox title="Meet the Team" description="Team description or link to page of individual members(?)" image={MemeTeam} id="memeteam"/>
     </div>
   );
 };
