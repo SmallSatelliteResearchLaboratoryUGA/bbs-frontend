01b5d808cd446eef71f890beb9c1c5e84c560897
QuintonHeald <qbh18213@uga.edu>
2023-11-26 19:22:00 -0500

    Renamed Components for Consistency

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
-import "../styles/About.css"
+import "../styles/Tutorial.css"
 import AboutBox from './AboutBox';
 import MemeTeam from "../assets/meme team.jpg"
 
 const Tutorial: React.FC = () => {
 
   return (
-    <div className={"about"}>
-      <div id="about-background" />
-      <h1 className="about-title" id="about-title">
+    <div className={"tutorial"}>
+      <div id="tutorial-background" />
+      <h1 className="tutorial-title" id="tutorial-title">
         BBS Tutorial
       </h1>
       <AboutBox title="Step 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." image={MemeTeam} id="memeteam"/>
