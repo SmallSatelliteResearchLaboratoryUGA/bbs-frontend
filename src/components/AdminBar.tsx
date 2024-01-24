c2d6b5a6a89537fa6c254ad657c766b140e74e55
Quinton Heald <qbh18213@uga.edu>
2024-01-24 16:32:24 -0500

    AdminBar links functional

--- src/components/AdminBar.tsx
+++ src/components/AdminBar.tsx
@@ -1,40 +1,15 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "../styles/AdminBar.css"
-import { Spin as Hamburger } from 'hamburger-react'
-
 
 const AdminBar: React.FC = () => {
-
-    const [isOpen, setOpen] = useState(false);
-    const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
     return (
 
-        <div className='sidenav active'>
-            <Hamburger 
-                        toggled={isOpen} 
-                        toggle={setOpen} 
-                        color={'#FFFFFF80'}
-                        hideOutline={false}
-                        onToggle={toggled => {
-                            if(toggled) {
-                                setShowHamburgerMenu(true);
-                            }
-                            else {
-                                setShowHamburgerMenu(false);
-                            }
-                        }}
-                    />
-            <ul>
-                <li>
-                    <a href='/'>Home</a>
-                </li>
-                <li>
-                    <a href='/about'>About</a>
-                </li>
-                <li>
-                    <a href='/bbs'>BBS</a>
-                </li>
-            </ul>
+        <div className={"sidenav active"}>
+            <Link to="/admin" className='link'>Admin Home</Link>
+            <Link to="/admin/users" className='link'>Verify Users</Link>
+            <Link to="/admin/verify-posts" className='link'>Verify Messages</Link>
+            <Link to="/admin/verify-posts" className='link'>Verify Images</Link>
         </div>
     );
 };
