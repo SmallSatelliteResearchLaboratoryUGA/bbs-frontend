bd47355d2359e56239240e7c668fd10db3d671fe
Quinton Heald <qbh18213@uga.edu>
2024-02-13 20:43:25 -0500

    Callsign specific posts visible on user page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import "../styles/Profile.css"
 import PostBox from './PostBox';
 import StickyNote from './StickyNote';
@@ -7,26 +6,34 @@ import { Post } from '../types';
 
 const Profile: React.FC = () => {
 
-  const { callsign } = useParams() as {callsign : string};
+  const [callsign] = useState<string>("testing"); // Set callsign to a predetermined value
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch('http://localhost:8000/sign', {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
+      try {
+        const res = await fetch(`http://localhost:8000/sign/${callsign}`, { // Use backticks for template literal
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+        if (!res.ok) {
+          throw new Error('Failed to fetch posts');
         }
-      });
-      const data: Post[] = JSON.parse(await res.json());
-      console.log("Data for posts object: " + data);
-      setPosts(data);
-      setLoading(false);
+        const data: Post[] = await res.json();
+        console.log("Data for posts object: " + data);
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } 
     };
-
-    fetchPosts()
-  }, []);
+    if (callsign) {
+      fetchPosts()
+    }
+  }, [callsign]);
   
   return (
     <div className={"user"}>
@@ -34,15 +41,15 @@ const Profile: React.FC = () => {
       <h1 className="user-title" id="user-title">
         Welcome!
       </h1>
-      <PostBox title="Approved Posts..." description=""/>
-      <PostBox title="Pending Posts..." description=""/>
-      <div className='sticky-note-container'>
+      <div className='post-container'>
         {loading || posts.length === 0 ? null : posts.map((post) => (
           <div key={post.id}>
             <StickyNote post={post} />
           </div>
         ))}
       </div>
+      <PostBox title="Approved Posts..." description=""/>
+      <PostBox title="Pending Posts..." description=""/>
     </div>
   );
 };
